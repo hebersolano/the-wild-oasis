24020254b9f897aa7f052f633d5b268b5e630960
Heber Solano <hebersolano1@gmail.com>
2024-03-15 22:36:40 -0600

    styled components 0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: aliceblue;
+`;
+
+const H1 = styled`
+  font-size: 20px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading type="h1">Hello World</Heading>
+        <Button>Check in</Button>
+        <Heading as="h2">Form</Heading>
+        <Input type="text" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ui/Input.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid var(--color-gray-300);
+  background-color: var(--color-gray-0);
+  border-radius: var(--border-radius-sm);
+  padding: var(--shadow-sm);
+`;
+
+export default Input;
